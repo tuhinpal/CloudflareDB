5e80f0bc5717ed4d6be242b500b8d2deaca7934a
Tuhin Kanti Pal <me@mailtuhin.ml>
2021-02-05 21:39:43 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,122 @@
+<h1 align="center">
+  <a href="https://github.com/cachecleanerjeet/CloudflareDB"><img src="https://telegra.ph/file/9883b51597e9c4f825269.png" alt="whatsbot" width="240"></a>
+  <br>
+</h1>
+<h3 align="center">The low latency database powered by Cloudflare Worker and KV</h3>
+<br><br>
+
+### *Features :*
+- Globally Available
+- Lowest Latancy
+- Serverless
+- Rest API
+- Token Based Authenticated Request Support
+
+### *API :*
+
+#### 1. Save a Payload (JSON)
+
+- Request:
+
+```
+Method: POST
+Content-Type: application/json
+URL (Secured with Token): https://<your-app>.workers.dev/?key=<post-key>
+URL (Not Secured): https://<your-app>.workers.dev/
+BODY: Json Data (Not Parsed)
+```
+
+- Response:
+```
+{
+    "status": true,
+    "_id": "16125252787azq",
+    "query": "https://<your-app>.workers.dev/16125252787azq",
+    "data": {
+        "i_am": "writing a readme"
+    }
+}
+```
+- Set an ID manually:
+```
+Send this 👇
+
+"_id":"my-unique-id"
+
+with Body
+```
+
+#### 2. Get a saved Data:
+
+- Request:
+
+```
+Method: GET
+URL: https://<your-app>.workers.dev/<_id>
+```
+
+- Response:
+
+```
+{
+    "status": true,
+    "_id": "16125252787azq",
+    "data": {
+        "i_am": "writing a readme"
+    }
+}
+```
+
+#### 3. Delete a Data:
+
+- Request:
+
+```
+Method: DELETE
+URL (Secured with Token): https://<your-app>.workers.dev/<_id>?key=<delete-key>
+URL (Not Secured): https://<your-app>.workers.dev/<_id>
+```
+
+- Response:
+
+```
+{
+    "status": true,
+    "msg": "Deleted Successfully"
+}
+```
+
+### *Deploy :*
+
+- Open [Cloudflare Worker](https://workers.cloudflare.com "Cloudflare Worker") Page
+- Click on KV
+- In <code>Namespace Name</code> section Type a Name & Click on <code>Add</code>, a namespace will created.
+- Now click on Workers & Create a worker
+- Copy the code of <code>[worker.js](https://github.com/cachecleanerjeet/CloudflareDB/blob/main/worker.js "worker.js")</code> , paste into Worker & Click on Save & Deploy
+- Now go back to worker main page, here you will see that your created worker listed there, click on that.
+- Click on <code>Settings</code>
+- In <code>KV Namespace Bindings</code> section click on <code>Add Binding</code>
+- Write <code>TUHIN</code> in Variable name & select your recently created Namespace for KV namespace.
+- You just made your Cloudflare DB, Now Read the API Endpoint and use it in your Project 😪
+
+### *Secure with Token :*
+*You can impliment token based authentication system for POST and DELETE requests*
+
+- Write your key in <code>POSTKEY</code> to secure POST requests with a Token (Line No: 12)
+- Write your key in <code>DELETEKEY</code> to secure DELETE requests with a Token (Line No: 13)
+
+### *Note for Kangers :*
+
+Kanging will not make you a developer. So, don't be a kanger ⚠
+
+### *License & Copyright :*
+- This Project is [Apache-2.0](https://github.com/cachecleanerjeet/CloudflareDB/blob/main/LICENSE) Licensed
+- Copyright 2021 by [Tuhin Kanti Pal](https://github.com/cachecleanerjeet)
+
+### *Connect :*
+- [Channel](https://telegram.dog/tprojects)
+- [Support Group](https://telegram.dog/t_projects)
+
+
+### Thanks to Cloudflare to give these services for free 🥰
+
---
+++ worker.js
@@ -0,0 +1,147 @@
+/*
+ Project-Name: Cloudflare DB
+ Author: Tuhin Kanti Pal
+ Author's Github: https://github.com/cachecleanerjeet
+ Author's Email: me@thetuhin.com
+ LICENSE: Apache-2.0 
+ Note for Kangers: Changing author's name will not make you a developer
+ Contact: https://telegram.dog/t_projects
+ Channel: https://telegram.dog/tprojects
+*/
+
+const POSTKEY = null; // "your-key" to secure POST requests with a token
+const DELETEKEY = null; // "your-key" to secure DELETE requests with a token
+
+async function handleRequest(request) {
+
+    const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,POST,DELETE,OPTIONS",
+        "Access-Control-Max-Age": "86400",
+        "Content-Type": "application/json",
+        "Cache-Control": "no-cache, no-store, must-revalidate",
+        "Your-IP": request.headers.get("cf-connecting-ip"),
+        "Your-Country": request.headers.get("CF-IPCountry"),
+        "Host": request.headers.get("host"),
+        "Made-By": atob('VHVoaW4gS2FudGkgUGFsLCBodHRwczovL2dpdGh1Yi5jb20vY2FjaGVjbGVhbmVyamVldA==')
+    }
+
+    if (request.method == "OPTIONS") { // Handel Preflight Requests
+        return new Response(null, {
+            status: 200,
+            headers
+        })
+
+    } else if (request.method == "POST") { // Insert a JSON Payload
+        if (new URL(request.url).searchParams.get('key') !== POSTKEY && POSTKEY !== null) {
+            return new Response(JSON.stringify({
+                status: false,
+                msg: "Unauthorized"
+            }), {
+                status: 401,
+                headers
+            })
+        } else {
+            var setpayload = await request.json()
+            if (setpayload._id == undefined) {
+                var keyid = (await (await fetch('https://time.akamai.com/')).text()) + Math.random().toString(36).substring(9)
+            } else {
+                var keyid = setpayload._id
+            }
+            await TUHIN.put(keyid, JSON.stringify(setpayload))
+            return new Response(JSON.stringify({
+                status: true,
+                _id: keyid,
+                query: `https://${request.headers.get("host")}/${keyid}`,
+                data: setpayload
+            }), {
+                status: 200,
+                headers
+            })
+        }
+    } else if (request.method == "GET") { // Get a JSON Data
+        var path = new URL(request.url).pathname
+        if (path == "/") {
+            return new Response(JSON.stringify({
+                status: "Running"
+            }), {
+                status: 200,
+                headers
+            })
+        } else {
+            var keyid = path.replace('/', '')
+            var getpayload = await TUHIN.get(keyid)
+            if (getpayload == null) {
+                return new Response(JSON.stringify({
+                    status: false,
+                    message: "Not Found"
+                }), {
+                    status: 200,
+                    headers
+                })
+            } else {
+                return new Response(JSON.stringify({
+                    status: true,
+                    _id: keyid,
+                    data: JSON.parse(getpayload)
+                }), {
+                    status: 200,
+                    headers
+                })
+            }
+        }
+    } else if (request.method == "DELETE") { // Delete a Document
+        if (new URL(request.url).searchParams.get('key') !== DELETEKEY && DELETEKEY !== null) {
+            return new Response(JSON.stringify({
+                status: false,
+                msg: "Unauthorized"
+            }), {
+                status: 401,
+                headers
+            })
+        } else {
+            var path = new URL(request.url).pathname;
+            if (path == '/') {
+                return new Response(JSON.stringify({
+                    status: false,
+                    message: "Can't Delete /"
+                }), {
+                    status: 500,
+                    headers
+                })
+            } else {
+                var test = await TUHIN.delete(path.replace('/', ''))
+                return new Response(JSON.stringify({
+                    status: true,
+                    msg: "Deleted Successfully"
+                }), {
+                    status: 200,
+                    headers
+                })
+            }
+        }
+    } else {
+        return new Response(JSON.stringify({
+            status: false,
+            message: "Only supports GET, POST, DELETE, OPTIONS"
+        }), {
+            status: 500,
+            headers
+        })
+    }
+}
+
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+})
+
+/*
+ Project-Name: Cloudflare DB
+ Author: Tuhin Kanti Pal
+ Author's Github: https://github.com/cachecleanerjeet
+ Author's Email: me@thetuhin.com
+ LICENSE: Apache-2.0
+ Note for Kangers: Changing author's name will not make you a developer
+ Contact: https://telegram.dog/t_projects
+ Channel: https://telegram.dog/tprojects
+*/
